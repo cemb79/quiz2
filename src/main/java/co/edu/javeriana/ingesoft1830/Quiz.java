9b54f58bd4c9e3c2dd0d6e6346aff063c5e9c315
Camilo Mercado <cmercado@emida.net>
2018-08-22 17:28:05 -0500

    Added new feature in billing

--- src/main/java/co/edu/javeriana/ingesoft1830/Quiz.java
+++ src/main/java/co/edu/javeriana/ingesoft1830/Quiz.java
@@ -4,5 +4,11 @@ public class Quiz {
 
 	public static void main(String[] args){
 		System.out.println("This is the first quiz");
+		Quiz quiz = new Quiz();
+		quiz.doBilling();
+	}
+
+	public void doBilling(){
+		System.out.println("Inside billing");
 	}
 }
